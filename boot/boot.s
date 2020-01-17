464c380803ab80bb677d33278334f2610489d7a4
Yan_XR <yan_xr@Yan-XRdeMacBook-Pro.local>
2020-01-17 14:54:41 +0800

    FIx:fix int, regs

--- boot/boot.s
+++ boot/boot.s
@@ -11,12 +11,12 @@ ljmp $BOOTSEG, $_bootst
 _bootst:
     mov $0x03,%ah
     int $0x10
+    mov $0x13, %ah
     mov $BOOTSEG, %cx
     mov %cx , %es
     mov $str_p, %bp
     mov $0x0003, %bx
     int $0x13
-	jmp _bootst
 
 .=510
 
