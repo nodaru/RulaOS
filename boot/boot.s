4435f90622819060ce3c36b8262bfaa4ce41ec6c
Yan_XR <yan_xr@Yan-XRdeMacBook-Pro.local>
2020-01-17 14:40:43 +0800

    Dev:show string when booting

--- boot/boot.s
+++ boot/boot.s
@@ -1,11 +1,21 @@
 .code16
 .global _bootst
 
+str_p:
+    .ascii "RulaOS\n"
+
 .equ BOOTSEG, 0x07c0
 
 ljmp $BOOTSEG, $_bootst
 
 _bootst:
+    mov $0x03,%ah
+    int $0x10
+    mov $BOOTSEG, %cx
+    mov %cx , %es
+    mov $str_p, %bp
+    mov $0x0003, %bx
+    int $0x13
 	jmp _bootst
 
 .=510
