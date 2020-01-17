22abbdd104b98f3b721d69ba04989f26aca50669
Yan_XR <yan_xr@Yan-XRdeMacBook-Pro.local>
2020-01-17 15:01:44 +0800

    Fix:fix section

--- boot/boot.s
+++ boot/boot.s
@@ -1,24 +1,24 @@
 .code16
 .global _bootst
 
-str_p:
-    .ascii "RulaOS\n"
+.text
+    str_p:
+        .ascii "RulaOS\n"
+    .equ BOOTSEG, 0x07c0
+    ljmp $BOOTSEG, $_bootst
+    _bootst:
+        mov $0x03,%ah
+        int $0x10
+        mov $0x13, %ah
+        mov $BOOTSEG, %cx
+        mov %cx , %es
+        mov $str_p, %bp
+        mov $0x0003, %bx
+        int $0x13
 
-.equ BOOTSEG, 0x07c0
+    _loop:
+        jmp _loop
+    .=510
 
-ljmp $BOOTSEG, $_bootst
-
-_bootst:
-    mov $0x03,%ah
-    int $0x10
-    mov $0x13, %ah
-    mov $BOOTSEG, %cx
-    mov %cx , %es
-    mov $str_p, %bp
-    mov $0x0003, %bx
-    int $0x13
-
-.=510
-
-id:
-	.word 0xaa55
+    id:
+        .word 0xaa55
